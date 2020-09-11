ddcf28e24ad0909b13287ebff1840b995d00cbdb
lee <lee-dasilva@hotmail.com>
2020-09-11 11:05:20 +0100

    usage of multiple source files

---
+++ mult_src_files/src/display.rs
@@ -0,0 +1,5 @@
+// by default the function are private, sooo
+// they can only be access to the module them self
+pub fn print_ms() {
+    println!("hello, how are you!");
+}
---
+++ mult_src_files/src/main.rs
@@ -0,0 +1,6 @@
+// mod will include the display source file
+mod display;
+
+fn main() {
+    display::print_ms();
+}
